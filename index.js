0ded0b788b2deb40c7a00f2ce252e4bf2edcea5c
K-lubna <164841387+K-lubna@users.noreply.github.com>
2026-05-12 12:47:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-import express from "express";
-import cors from "cors";
 import dotenv from "dotenv";
 dotenv.config();
+import express from "express";
+import cors from "cors";
+
 const app = express();
 
 app.use(cors());
